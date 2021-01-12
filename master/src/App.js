2a77607d8f97287e4ebe2e4eb2274307848cf220
Daniel Svitan <dankosaurus9@gmail.com>
2021-01-12 17:04:48 +0100

    initial commit

---
+++ master/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <div className="sub-domains">
+            <a href={"http://localhost:3000/"} className={"subdomain"}>Home</a>
+            <p className={"subdomain-splitter"}> / </p>
+            <a href={"http://localhost:3002/"} className={"subdomain"}>Courses</a>
+            <p className={"subdomain-splitter"}> / </p>
+            <a href={"http://localhost:3004/"} className={"subdomain"}>Contacts</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
