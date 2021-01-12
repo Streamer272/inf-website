e7c72798a195592151e1d6262c2ba407f588185e
Daniel Svitan <dankosaurus9@gmail.com>
2021-01-12 15:41:34 +0100

    :tada: initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <div className="sub-domains">
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
