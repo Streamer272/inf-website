6e6c7255ca4d9a147cafde7693a3bbb6154c96b4
Daniel Svitan <dankosaurus9@gmail.com>
2021-01-15 10:37:22 +0100

    adding react router

--- master/src/App.js
+++ master/src/App.js
@@ -1,19 +1,11 @@
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Home } from "./Components/Home.js";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <h1 className={"title"}>Home</h1>
-        <div className="sub-domains">
-          <a href={"http://localhost:3000/"} className={"subdomain"}>Home</a>
-          <p className={"subdomain-splitter"}>/ </p>
-          <a href={"http://localhost:3002/"} className={"subdomain"}>Courses</a>
-          <p className={"subdomain-splitter"}>/ </p>
-          <a href={"http://localhost:3004/"} className={"subdomain"}>Contacts</a>
-        </div>
-      </div>
-    </div>
+    <Router>
+      <Route path={"/"} exact={true} component={ Home }/>
+    </Router>
   );
 }
 
