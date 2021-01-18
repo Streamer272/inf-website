a7e8fb8abf82345a0117529a2ebb87357ee976b0
Daniel Svitan <dankosaurus9@gmail.com>
2021-01-18 14:06:30 +0100

    making code look better

--- master/src/script.js
+++ master/src/script.js
@@ -1,70 +1,77 @@
 let titleAnimationRunning = false;
-let oldScroll = 0;
 let globalOpacity = 1;
 
-window.onscroll = () => {
+
+const titleAnimation = () => {
     const scroll = window.scrollY;
 
-    if (scroll >= 150) {
-        let title = document.getElementById("title");
-
-        if (!titleAnimationRunning) {
-            if (globalOpacity === 0) { return }
-            let opacity = 1.0;
-
-            const frame = () => {
-                if (window.scrollY >= 150) {
-                    if (opacity <= 0) {
-                        title.style.opacity = "0";
-                        titleAnimationRunning = false;
-                        globalOpacity = 0;
-                        clearInterval(id);
-                    } else {
-                        opacity -= 0.01;
-                        title.style.opacity = opacity;
-                    }
-                } else {
-                    title.style.opacity = "0";
-                    titleAnimationRunning = false;
-                    globalOpacity = 0;
-                    clearInterval(id);
-                }
-            }
-
-            titleAnimationRunning = true;
-            const id = setInterval(frame, 10);
-        }
-
-    } else {
-        let title = document.getElementById("title");
-
-        if (!titleAnimationRunning) {
-            if (globalOpacity === 1) { return }
-            let opacity = 0;
-
-            const frame = () => {
-                if (window.scrollY < 150) {
-                    if (opacity >= 1) {
-                        title.style.opacity = "1";
-                        titleAnimationRunning = false;
-                        globalOpacity = 1;
-                        clearInterval(id);
-                    } else {
-                        opacity += 0.01;
-                        title.style.opacity = opacity;
-                    }
-                } else {
-                    title.style.opacity = "1";
-                    titleAnimationRunning = false;
-                    globalOpacity = 1;
-                    clearInterval(id);
-                }
-            }
-
-            titleAnimationRunning = true;
-            const id = setInterval(frame, 10);
+    const getScrollRight = (state) => {
+        if (state === "down") {
+            return window.scrollY >= 150;
+        } else {
+            return window.scrollY < 150;
         }
     }
 
-    oldScroll = scroll;
+    const getAnimationFinished = (state, opacity) => {
+        if ("down" === state) {
+            return opacity <= 0;
+        } else {
+            return opacity >= 1;
+        }
+    }
+
+    let type;
+    let globalOpacityEquation;
+    let startOpacity;
+    let stopOpacity;
+    let mathOpacity;
+
+    if (scroll >= 150) {
+        type = "down";
+        globalOpacityEquation = globalOpacity === 0;
+        startOpacity = 1.0;
+        stopOpacity = "0";
+        mathOpacity = -0.01;
+    } else {
+        type = "up";
+        globalOpacityEquation = globalOpacity === 1;
+        startOpacity = 0;
+        stopOpacity = "1";
+        mathOpacity = 0.01;
+    }
+
+    let title = document.getElementById("title");
+
+    if (!titleAnimationRunning) {
+        if (globalOpacityEquation) { return }
+        let opacity = startOpacity;
+
+        const frame = () => {
+            if (getScrollRight(type)) {
+                if (getAnimationFinished(type, opacity)) {
+                    title.style.opacity = stopOpacity;
+                    titleAnimationRunning = false;
+                    globalOpacity = parseInt(stopOpacity);
+                    clearInterval(id);
+                } else {
+                    opacity += mathOpacity;
+                    title.style.opacity = opacity;
+                }
+            } else {
+                title.style.opacity = stopOpacity;
+                titleAnimationRunning = false;
+                globalOpacity = stopOpacity;
+                clearInterval(id);
+            }
+        }
+
+        titleAnimationRunning = true;
+        const id = setInterval(frame, 10);
+    }
+}
+
+
+window.onscroll = () => {
+    titleAnimation();
 }
