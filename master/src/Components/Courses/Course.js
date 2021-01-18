65fff6809924500f5f85b5a9b798da1ab42dd1d8
Daniel Svitan <dankosaurus9@gmail.com>
2021-01-18 18:04:59 +0100

    making course look better, but there is css problem

--- master/src/Components/Courses/Course.js
+++ master/src/Components/Courses/Course.js
@@ -1,10 +1,24 @@
 import "./Course.css";
 
+const CourseSubjects = ({ subjects }) => {
+    const listItems = subjects.map((subject) =>
+        <li className={ "subject" }>{ subject }</li>
+    );
+
+    return (
+        <div className={ "subjects-div" }>
+            <ul>{ listItems }</ul>
+        </div>
+    );
+}
+
 const Course = ({ courseName, coursePrice, courseSubjects }) => {
     return (
         <div className={ "course" }>
             <p className={ "course-name" }>{ courseName }</p>
             <p className={ "course-price" }>{ coursePrice }€</p>
+            <br /><br /><br /><br />
+            <CourseSubjects subjects={ courseSubjects } />
         </div>
     );
 }
